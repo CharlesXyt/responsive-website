bc421ccf60c7fb7ff6af5b835f0d4ff976ba4004
charlesxyt <charles.yitong@gmail.com>
2020-01-22 17:34:51 +1100

    Updates

---
+++ precache-manifest.7d9bd20e1e54175054548a83aab4f029.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9e2051f1ebab7a87bba8bc8ca6b6ec",
+    "url": "/responsive-website/index.html"
+  },
+  {
+    "revision": "d5a56a817ab201f66462",
+    "url": "/responsive-website/static/css/main.fc7884a6.chunk.css"
+  },
+  {
+    "revision": "f9357e88a597031a9dac",
+    "url": "/responsive-website/static/js/2.0120ad37.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/responsive-website/static/js/2.0120ad37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5a56a817ab201f66462",
+    "url": "/responsive-website/static/js/main.44d103a7.chunk.js"
+  },
+  {
+    "revision": "c90504eb03927fc6d45c",
+    "url": "/responsive-website/static/js/runtime-main.8b193010.js"
+  },
+  {
+    "revision": "6923d550a41d50c026d7e765afd78af5",
+    "url": "/responsive-website/static/media/hero.6923d550.jpg"
+  },
+  {
+    "revision": "93f9c92588a66b37be5b7ec4bc950858",
+    "url": "/responsive-website/static/media/logo-white.93f9c925.png"
+  }
+]);
