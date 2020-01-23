45fd5f046a7d166ca540b2516c57b65eca857df1
charlesxyt <charles.yitong@gmail.com>
2020-01-23 12:26:02 +1100

    add how it works part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import HomePage from './container/HomePage/HomePage'
+import HowItWorks from './container/HowItWorks/HowItWorks'
+import OurCities from './container/OurCities/OurCities'
 import {Switch,Route} from 'react-router-dom'
 
 function App() {
   return (
     <React.Fragment>
       <Switch>
-        <Route path="/how" component={HomePage}/>
-        <Route path="/cities" component={HomePage}/>
+        <Route path="/how" component={HowItWorks}/>
+        <Route path="/cities" component={OurCities}/>
         <Route path="/signup" component={HomePage}/>
         <Route exact path="/" component={HomePage}/>
       </Switch>
